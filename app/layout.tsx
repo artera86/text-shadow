f0152c8716e8108a651ab9dc3f785c54a2eff733
artera86 <135210425+artera86@users.noreply.github.com>
2024-06-12 10:40:00 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   }
   @return $val;
 }</style></head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}></body>
     </html>
   );
 }
